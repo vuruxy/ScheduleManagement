b67e16ada5aad43b831bfa854fe55096cd7d1e38
zanhtet <nyuntzanhtet@gmail.com>
2021-04-13 21:23:41 +0800

    Project Structure

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+const homeController = require('./controllers/home');
+const config = require('./config.json');
+
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'main',
+    helpers: {
+        url: () => {
+            return 'http://' + config.host + ':' + config.port;
+        }
+    }
+}));
+
+app.use(session({secret: 'test'}));
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(express.static(__dirname + '/views/public'));
+
+app.use('/', homeController);
+
+app.listen(config.port, () => {
+    console.log('Applicaton Start');
+});
