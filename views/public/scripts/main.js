128c58a4590e82562a6b64faca6dc695883f0257
zanhtet <nyuntzanhtet@gmail.com>
2021-04-14 20:46:30 +0800

    css changes for login

--- views/public/scripts/main.js
+++ views/public/scripts/main.js
@@ -278,7 +278,32 @@
     
     };
 
-   
+   /* Smooth Scrolling
+    * ------------------------------------------------------ */
+    var clSmoothScroll = function() {
+        
+        $('.smoothscroll').on('click', function (e) {
+            var target = this.hash,
+            $target    = $(target);
+            
+                e.preventDefault();
+                e.stopPropagation();
+
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, cfg.scrollDuration, 'swing').promise().done(function () {
+
+                // check if menu is open
+                if ($('body').hasClass('menu-is-open')) {
+                    $('.header-menu-toggle').trigger('click');
+                }
+
+                window.location.hash = target;
+            });
+        });
+
+    };
+
 
    /* Placeholder Plugin Settings
     * ------------------------------------------------------ */
@@ -433,6 +458,7 @@
         clStatCount();
         clMasonryFolio();
         clSlickSlider();
+        clSmoothScroll();
         clPlaceholder();
         clAlertBoxes();
         clContactForm();
