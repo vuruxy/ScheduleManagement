aa13cfb295c5988ff3ca61a8486c7e2e40aa39d4
vuruxy <kaunghtetoo07@gmail.com>
2021-04-19 21:56:56 +0800

    role.js

--- models/role.js
+++ models/role.js
@@ -11,9 +11,9 @@ module.exports = {
 		});
 	},
 	setRoles: async (role) => {
-		let sql = "INSERT INTO role(roleId, name, viewReport, createJob, assignJob)" +
+		let sql = "INSERT INTO role(roleId, name, viewReport, canCreateJob, assignJob)" +
 			"Value(?,?,?,?,?)";
-		let param = [role.roleId, role.name, role.viewReport, role.createJob, role.assignJob];
+		let param = [role.roleId, role.name, role.viewReport, role.canCreateJob, role.assignJob];
 		return await dbUtli.executeSql(sql, param).then(fields => {
 			return true;
 		});
@@ -29,7 +29,7 @@ module.exports = {
 		let sql = "UPDATE role SET roleName = ?, canViewReport = ? , canCreateEmployee = ?, canCreateJob = ? , canAssginJob =? " +
 			"WHERE roleId = ?)"
 
-		let param = [role.roleName, role.canViewReport, role.canCreateEmployee, role.canAssginJob, role.roleId];
+		let param = [role.roleName, role.canViewReport, role.canCreateEmployee, role.canAssginJob, role.canCreateJob, role.roleId];
 
 		return await dbUtli.executeSql(sql, param).then(fields => {
 			return true;
